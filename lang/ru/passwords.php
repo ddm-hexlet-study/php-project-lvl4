9be515e4c1018386fe8e8f2f22157991a1c6e60d
Dmitry <ddm14159@gmail.com>
2022-05-04 18:40:35 +0300

    locale

---
+++ lang/ru/passwords.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Ваш пароль был сброшен!',
+    'sent' => 'Мы отправили ссылку для сброса пароля по электронной почте!',
+    'throttled' => 'Пожалуйста, подождите, прежде чем повторить попытку.',
+    'token' => 'Этот токен сброса пароля недействителен.',
+    'user' => 'Мы не можем найти пользователя с таким адресом электронной почты.',
+    'Reset Password' => 'Сброс пароля',
+    'Email Address' => 'Email',
+    'Send Password Reset Link' => 'Сбросить пароль',
+    'Login' => 'Вход',
+    'Password' => 'Пароль',
+    'Remember me' => 'Запомнить меня',
+];
