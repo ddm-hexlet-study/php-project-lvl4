bd023141188097429a91621740289e2116dc2ef9
Dmitry <ddm14159@gmail.com>
2022-05-06 20:59:15 +0300

    refactoring

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            TaskStatusSeeder::class,
+            TaskStatusesSeeder::class,
         ]);
     }
 }
